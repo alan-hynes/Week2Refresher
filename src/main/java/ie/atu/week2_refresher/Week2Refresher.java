d12ef3845e7720e868a383758857d97eef99bb79
G00400498@atu.ie <g00400498@atu.ie>
2024-09-23 15:50:53 +0100

    Populated Week2Refresher Class and created Product class

--- src/main/java/ie/atu/week2_refresher/Week2Refresher.java
+++ src/main/java/ie/atu/week2_refresher/Week2Refresher.java
@@ -1,4 +1,26 @@
 package ie.atu.week2_refresher;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Week2Refresher {
+    @GetMapping("/hello")
+    public String message()
+    {
+        return "Hello";
+    }
+    @GetMapping("/hello/{name}")
+    public String myName(@PathVariable String name)
+    {
+        return "Your name is " + name;
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam String search1, @RequestParam(defaultValue = "") String search2)
+    {
+        return search1 + " " + search2;
+    }
 }
